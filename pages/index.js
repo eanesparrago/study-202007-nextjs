4666e44f92ee01d6fc6d113d195f00359df1bf21
iansanwich <iansanwich@gmail.com>
2020-07-23 08:51:53 +0800

    Rename heading

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home({ allPostsData }) {
       <Head>…</Head>
       <section className={utilStyles.headingMd}>…</section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <h2 className={utilStyles.headingLg}>Blog (Preview branch)</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
